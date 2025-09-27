9f829a1c2b1cbf4a0358bb8daca106cc590fa095
Bhavyai Gupta <zbhavyai@gmail.com>
2025-09-26 19:19:27 -0600

    fix ping endpoint in tests

--- tests/api/routes/test_ping_router.py
+++ tests/api/routes/test_ping_router.py
@@ -2,6 +2,6 @@ from fastapi.testclient import TestClient
 
 
 def test_ping(client: TestClient) -> None:
-    response = client.get("/ping")
+    response = client.get("/api/ping")
     assert response.status_code == 200
     assert response.text == "pong\n"
