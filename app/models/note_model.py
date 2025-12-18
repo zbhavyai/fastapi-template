57f4167dad90263a353f01e1c58840a570a3415a
Bhavyai Gupta <zbhavyai@gmail.com>
2025-12-18 15:39:13 -0700

    change max length to 120

--- app/models/note_model.py
+++ app/models/note_model.py
@@ -15,9 +15,7 @@ class Note(Base):
     title = Column(String(255), nullable=False)
     content = Column(Text, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    updated_at = Column(
-        DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False
-    )
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     __mapper_args__ = {
         "version_id_col": optlock,
